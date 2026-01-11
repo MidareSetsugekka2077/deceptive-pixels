c351093a913d0d4f32f9b41d3e6424547a8e46bb
Aidan Yip <aidanyip29@gmail.com>
2026-01-11 22:21:59 +0000

    configured custom colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,53 @@ module.exports = {
     extend: {
       colors: {
         border: 'var(--border)',
-        ring: 'var(--ring)',
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        card: 'var(--card)',
+        'card-foreground': 'var(--card-foreground)',
+        popover: 'var(--popover)',
+        'popover-foreground': 'var(--popover-foreground)',
+
+        primary: {
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-foreground)',
+        },
+        secondary: {
+          DEFAULT: 'var(--secondary)',
+          foreground: 'var(--secondary-foreground)',
+        },
+        muted: {
+          DEFAULT: 'var(--muted)',
+          foreground: 'var(--muted-foreground)',
+        },
+        accent: {
+          DEFAULT: 'var(--accent)',
+          foreground: 'var(--accent-foreground)',
+        },
+        destructive: {
+          DEFAULT: 'var(--destructive)',
+          foreground: 'var(--destructive-foreground)',
+        },
+
+        input: 'var(--input)',
+        'input-background': 'var(--input-background)',
+        'switch-background': 'var(--switch-background)',
+
+        ring: 'var(--ring)',
+      },
+
+      borderRadius: {
+        DEFAULT: 'var(--radius)',
+        lg: 'var(--radius)',
+      },
+
+      fontSize: {
+        base: 'var(--font-size)',
+      },
+
+      fontWeight: {
+        normal: 'var(--font-weight-normal)',
+        medium: 'var(--font-weight-medium)',
       },
     },
   },
