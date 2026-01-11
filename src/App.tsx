f05babcff2ad356629f606fb8e34c18138e31c07
Aidan Yip <aidanyip29@gmail.com>
2026-01-11 22:22:11 +0000

    cleaned up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from './components/ui/tabs';
-import { PrototypeTwo } from './PrototypeTwo';
+import { Prototype } from './Prototype';
 
 export default function App() {
   return (
     <div className="size-full bg-muted p-8 overflow-auto">
       <div className="max-w-7xl mx-auto">
         <header className="mb-8">
-          <h1 className="mb-2">Image Classification Game Prototype</h1>
-          <p className="text-muted-foreground">
-            Prototypes exploring different game mechanics for teaching Vision Transformers & Convolutional Neural Networks
-          </p>
+          <h1 className="mb-2">Deceptive Pixels</h1>
         </header>
-
-        <Tabs defaultValue="prototype1" className="w-full">
-          <TabsList className="grid w-full grid-cols-3 mb-6">
-            <TabsTrigger value="prototype2">Prototype Game</TabsTrigger>
-          </TabsList>
-
-          <TabsContent value="prototype2">
-            <PrototypeTwo />
-          </TabsContent>
-
-        </Tabs>
+        <Prototype />
       </div>
     </div>
   );
