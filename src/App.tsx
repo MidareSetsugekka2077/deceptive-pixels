9827712da2de9f793b6848587308658b44c55371
Aidan Yip <aidanyip29@gmail.com>
2026-01-17 17:41:53 +0000

    Centered Game Title

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ export default function App() {
   return (
     <div className="size-full bg-muted p-8 overflow-auto">
       <div className="max-w-7xl mx-auto">
-        <header className="mb-8">
-          <h1 className="mb-2">Deceptive Pixels</h1>
+        <header className="mb-8 text-center">
+          <h1 className="mb-2 text-3xl font-semibold">Deceptive Pixels</h1>
         </header>
         <Prototype />
       </div>
