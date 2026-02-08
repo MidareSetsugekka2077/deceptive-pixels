d2aaa49cb0d7d9048e6a4ac5a9fa607aa7640930
Aidan Yip <aidanyip29@gmail.com>
2026-02-08 05:47:42 +0000

    add router to naviagte to different attacks from title screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { useState } from 'react';
-import { MainGame } from './MainGame';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { TitleScreen } from './TitleScreen';
+import { PixelAttack } from './PixelAttack';
+import { RotateAttack } from './RotateAttack';
+import { ShiftAttack } from './ShiftAttack';
 
 export default function App() {
-  const [view, setView] = useState<'title' | 'game'>('title');
-
-  if (view === 'game') {
-    return <MainGame />;
-  }
+  const navigate = useNavigate();
 
   return (
-    <TitleScreen onPlay={() => setView('game')} />
+    <Routes>
+      <Route
+        path="/"
+        element={<TitleScreen onPlay={() => navigate('/pixel-attack')} />}
+      />
+      <Route path="/pixel-attack" element={<PixelAttack />} />
+      <Route path="/rotate-attack" element={<RotateAttack />} />
+      <Route path="/shift-attack" element={<ShiftAttack />} />
+    </Routes>
   );
 }
